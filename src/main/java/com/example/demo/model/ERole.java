3c0c720a25c8cdb6fb75b0fae70b9c9135e13810
Boom Boom <boomlee97@gmail.com>
2023-03-06 15:53:58 +0000

    Revert "Merge branch 'dev' into 'main'"
    This reverts merge request !3

--- src/main/java/com/example/demo/model/ERole.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.model;
-
-public enum ERole {
-    ROLE_CUSTOMER,
-    ROLE_RENTAL,
-    ROLE_ADMIN
-}
--- src/main/java/com/example/demo/service/AccountDetailsService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-public interface AccountDetailsService {
-    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
-}
